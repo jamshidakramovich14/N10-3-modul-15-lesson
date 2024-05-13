7a7dfb28dfec269e8b8a885084e6b60170badc74
jamshidakramovich14 <jamshidturdiyev761@gmail.com>
2024-05-13 20:43:27 +0500

    N10 3-modul 15-lesson

---
+++ script.js
@@ -0,0 +1,110 @@
+"use strict"
+
+
+
+// -------------------- 1-masala start --------------------
+
+// let userData = {
+//     id: '1',
+//     name: "Leanne Graham",
+//     username: "Bret",
+//     email: "Sincere@april.biz",
+// };
+
+// localStorage.setItem('userData', JSON.stringify(userData));
+
+
+
+
+// let storedData = localStorage.getItem('userData');
+// let foydalanuvchiMalumoti = JSON.parse(storedData);
+
+// console.log(userData);
+
+
+// -------------------- 1-masala end --------------------
+
+
+
+
+
+
+
+// -------------------- 2-masala start --------------------
+
+// const email = prompt('Emailingizni kiriting:');
+// const name = prompt('Ismingizni kiriting:');
+// const username = prompt('Usernameingizni kiriting:');
+
+// const userData = {
+//   email: email,
+//   name: name,
+//   username: username,
+// };
+
+// localStorage.setItem('userData', JSON.stringify(userData));
+
+// -------------------- 2-masala end --------------------
+
+
+
+
+
+
+// -------------------- 3-masala start --------------------
+
+// const newUser = {};
+
+// newUser.email = prompt('Emailingizni kiriting:');
+// newUser.name = prompt('Ismingizni kiriting:');
+// newUser.username = prompt('Usernameingizni kiriting:');
+
+// localStorage.setItem('newUser', JSON.stringify(newUser));
+
+// -------------------- 3-masala end --------------------
+
+
+
+
+
+
+// -------------------- 4-masala start --------------------
+
+// const usernameToDelete = prompt('O\'chirish uchun username ni kiriting:');
+
+// const storedData = localStorage.getItem('userData');
+// const userData = JSON.parse(storedData);
+
+// if (userData && userData.username === usernameToDelete) {
+//   localStorage.removeItem('userData');
+//   console.log('Foydalanuvchi ma\'lumotlari o\'chirildi.');
+// } else {
+//   console.log('Foydalanuvchi ma\'lumotlari topilmadi yoki username noto\'g\'ri kiritildi.');
+// }
+
+// -------------------- 4-masala end --------------------
+
+
+
+
+
+
+
+
+// -------------------- 5-masala start --------------------
+
+// const clearButton = document.createElement('button');
+// clearButton.textContent = 'Clear All';
+// document.body.appendChild(clearButton);
+
+// clearButton.addEventListener('click', () => {
+//   const confirmation = confirm('Haqiqatan ham barcha ma\'lumotlarni o\'chirmoqchimisiz?');
+//   if (confirmation) {
+//     localStorage.clear();
+//     console.log('Barcha ma\'lumotlar tozalandi.');
+//   } else {
+//     console.log('Ma\'lumotlar tozalanmadi.');
+//   }
+// });
+
+// -------------------- 5-masala end --------------------
